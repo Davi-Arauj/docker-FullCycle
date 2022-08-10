fcd839e088453628b30eb7992569e307bb4488d9
Davi-Arauj <moreiradaavi@gmail.com>
2022-08-10 10:16:46 -0300

    Container node salvando dados no container Mysql

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const porta = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Davi')`
+connection.query(sql)
+connection.end()
 
 app.get('/',(req,res) =>{
     res.send('<h1>Full Cycle</h1>')
